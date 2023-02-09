6c2cb98e35d74e18d7b9d763b604acecdebfd20b
Ana Carolina <desarrolladorpaykind@gmail.com>
2023-02-09 10:50:50 -0500

    Agregar archivos de proyecto.

---
+++ BE-CrudMascotas/Migrations/20230206204631_Primerajuste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BECrudMascotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primerajuste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BE-CrudMascotas/Models/AplicationBbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BE_CrudMascotas.Models
+{
+    public class AplicationBbContext : DbContext
+    {
+        public AplicationBbContext(DbContextOptions<AplicationBbContext> options) : base(options) 
+        { 
+
+        }  
+        
+        public DbSet<Mascota> Mascotas { get; set; }
+
+
+
+    }
+}
